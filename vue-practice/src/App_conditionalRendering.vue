d933aae522b095fccebd7d0c436e4bf3fead4bc2
shypang <smartdragon417@gmail.com>
2022-10-05 23:55:58 +0900

    conditional rendering

---
+++ vue-practice/src/App_conditionalRendering.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <!-- <h2 v-if="showName">My name is {{ user.name }}</h2>
+    <h2 v-else>이름을 보여줄 수 없습니다.</h2>
+    <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
+    <h2 v-else-if="user.age > 14 && user.age < 20">당신은 청소년입니다.</h2>
+    <h2 v-else>당신은 어린이입니다.</h2> -->
+    <h2 v-if="showName">{{ user.name }} IF</h2>
+    <h2 v-show="showName">{{ user.name }} Show</h2>
+
+    <ul>
+      <template v-if="question === 'frontend'">
+        <li>HTML은 재밌나요?</li>
+        <li>CSS는 재밌나요?</li>
+        <li>Javascript는 재밌나요?</li>
+      </template>
+
+      <template v-else>
+        <li>java은 재밌나요?</li>
+        <li>python는 재밌나요?</li>
+        <li>c#는 재밌나요?</li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      question: "back",
+      showName: false,
+      user: {
+        name: "scalper",
+        age: 16,
+        job: "programmer",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+.text-red {
+  color: red;
+}
+.text-green {
+  color: green;
+}
+.highlight {
+  font-weight: bold;
+  background: pink;
+}
+
+.line-through {
+  text-decoration: line-through;
+}
+</style>
---
+++ vue-practice/src/App_style_binding2.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1>Hello Vue!</h1>
+    <h2 class="line-through">line-through</h2>
+    <h2 v-bind:class="textDecoration" class="text-red">line-through</h2>
+    <h2 :class="isDone === true ? 'line-through' : 'highlight'">
+      line-through
+    </h2>
+    <h2
+      :class="{
+        highlight: isDone === false,
+        'text-red': username === 'scalper',
+      }"
+    >
+      Object형태의 동적 클래스
+    </h2>
+    <h2
+      :class="[
+        isDone === true ? 'line-through' : 'highlight',
+        username === 'scalper' ? 'text-red' : 'text-green',
+      ]"
+    >
+      Array 형태의 동적 클래스 부여
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      username: "scalper",
+      isDone: false,
+      textDecoration: "line-through",
+    };
+  },
+};
+</script>
+
+<style>
+.text-red {
+  color: red;
+}
+.text-green {
+  color: green;
+}
+.highlight {
+  font-weight: bold;
+  background: pink;
+}
+
+.line-through {
+  text-decoration: line-through;
+}
+</style>
