b1271e99359637346008cca9d52bef264053ecd5
shypang <smartdragon417@gmail.com>
2022-10-08 19:01:55 +0900

    deploy

--- vue-practice/src/components/composition/TestComponent.vue
+++ vue-practice/src/components/composition/TestComponent.vue
@@ -1,11 +1,72 @@
 <template>
-  <h2>Hello default</h2>
+  <h2>hello composition api</h2>
+  <h3>{{ name }}</h3>
+  <div>
+    price <input type="number" v-model="price" />amount
+    <input type="number" v-model="amount" />
+    <h3>Total Price :: {{ totalPrice }}</h3>
+  </div>
+  <hr />
+  <div>
+    first<input type="text" v-model="home.city" /> last
+    <input type="text" v-model="last" />
+    <h3>Full Name :: {{ fullName }}</h3>
+  </div>
 </template>
 
 <script>
-import {} from "vue";
+import { ref, computed, reactive, toRefs } from "vue";
 export default {
   name: "TestComponent",
+  setup() {
+    const username = ref("scalper");
+    const price = ref(5000);
+    const amount = ref(1);
+    const totalPrice = computed(() => {
+      return price.value + amount.value;
+    });
+    watch(price, (newValue, oldValue) => {
+      console.log(newValue, oldValue);
+    });
+    const state = reactive({
+      first: "Code",
+      last: "Scalper",
+      home: {
+        city: "Seoul",
+        type: "Apartment",
+      },
+    });
+    const fullName = computed(function () {
+      return `${state.first} ${state.last}`;
+    });
+    watch(
+      () => {
+        return state.first;
+      },
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      }
+    );
+
+    watch(
+      () => {
+        return { ...state.home };
+      },
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      },
+      { deep: true }
+    );
+    return {
+      username,
+      price,
+      amount,
+      totalPrice,
+      ...toRefs(state),
+      fullName,
+      Code,
+    };
+  },
 };
 </script>
 
